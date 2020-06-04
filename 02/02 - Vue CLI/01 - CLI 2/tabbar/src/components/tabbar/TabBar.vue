1c144d338695d10794b13529263f82758c3439aa
Zx <125139887@qq.com>
2020-06-04 21:42:50 +0800

    2020-06-04 晚上

--- 02/02 - Vue CLI/01 - CLI 2/tabbar/src/components/tabbar/TabBar.vue
+++ 02/02 - Vue CLI/01 - CLI 2/tabbar/src/components/tabbar/TabBar.vue
@@ -1,31 +1,16 @@
 <template>
-    <div id='tab-bar'>
-        <div class="tab-bar-item">
-            <img src="../../assets/img/tabbar/home.svg" alt="">
-            <div>首页</div>
-        </div>
-        <div class="tab-bar-item">
-            <img src="../../assets/img/tabbar/category.svg" alt="">
-            <div>分类</div>
-        </div>
-        <div class="tab-bar-item">
-            <img src="../../assets/img/tabbar/shopcart.svg" alt="">
-            <div>购物车</div>
-        </div>
-        <div class="tab-bar-item">
-            <img src="../../assets/img/tabbar/profile.svg" alt="">
-            <div>我的</div>
-        </div>
+    <div id="tab-bar">
+        <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'TabBar'
-    }
+        name: "TabBar"
+    };
 </script>
 
-<style>
+<style scoped>
     #tab-bar {
         display: flex;
         background-color: #f6f6f6;
@@ -35,17 +20,6 @@
         right: 0;
         bottom: 0;
 
-        box-shadow: 0px -3px 1px rgba(100, 100, 100, .1);
-    }
-
-    .tab-bar-item {
-        flex: 1;
-        text-align: center;
-        height: 49px;
-    }
-
-    .tab-bar-item img {
-        width: 24px;
-        height: 24px;
+        box-shadow: 0px -3px 1px rgba(100, 100, 100, 0.1);
     }
 </style>
