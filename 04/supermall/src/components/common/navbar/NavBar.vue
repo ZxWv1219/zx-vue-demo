bb466f04f43da36c2bedfedf747fdaa91f4fce09
周子兴 <125139887@qq.com>
2020-06-09 17:16:15 +0800

    2020-06-09 下午

--- 04/supermall/src/components/common/navbar/NavBar.vue
+++ 04/supermall/src/components/common/navbar/NavBar.vue
@@ -1,30 +1,30 @@
 <!--  -->
 <template>
-    <div class id="home">
-        <nav-bar class="home-nav">
-            <div slot="center">购物车</div>
-        </nav-bar>
-        <h2>Home</h2>
-        <div>{{homeMultidata}}</div>
+    <div class="nav-bar">
+        <div class="left">
+            <slot name='left'></slot>
+        </div>
+        <div class="center">
+            <slot name='center'></slot>
+        </div>
+        <div class="right">
+            <slot name='right'></slot>
+        </div>
     </div>
 </template>
 
 <script>
     //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
     //例如：import 《组件名称》 from '《组件路径》';
-    import NavBar from '@/components/common/navbar/NavBar'
-    import { getHomeMultidata } from '@/network/homeService'
 
     export default {
         //import引入的组件需要注入到对象中才能使用
-        components: {
-            NavBar
-        },
+        components: {},
         data() {
             //这里存放数据
             return {
-                homeMultidata: null
-            }
+
+            };
         },
         //监听属性 类似于data概念
         computed: {},
@@ -36,10 +36,7 @@
         },
         //生命周期 - 创建完成（可以访问当前this实例）
         created() {
-            getHomeMultidata().then(res => {
-                console.log(res)
-                // this.homeMultidata = res
-            })
+
         },
         //生命周期 - 挂载完成（可以访问DOM元素）
         mounted() {
@@ -55,8 +52,21 @@
     }
 </script>
 <style scoped>
-    .home-nav {
-        background-color: var(--color-tint);
-        color: #ffffff;
+    .nav-bar {
+        display: flex;
+        height: 44px;
+        line-height: 44px;
+        text-align: center;
+        box-shadow: 0 1px 1px rgba(100, 100, 100, 0.1);
+    }
+
+    .left,
+    .right {
+        width: 60px;
+    }
+
+    .center {
+        flex: 1;
+
     }
 </style>
