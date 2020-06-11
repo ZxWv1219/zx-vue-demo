4ec43f5d0aaa18b4051f827f3c25f2c9607acd30
周子兴 <125139887@qq.com>
2020-06-11 18:04:22 +0800

    2020-06-11 下午

--- 04/supermall/src/views/home/Home.vue
+++ 04/supermall/src/views/home/Home.vue
@@ -4,44 +4,15 @@
         <nav-bar class="home-nav">
             <div slot="center">购物车</div>
         </nav-bar>
-        <home-swiper :banners='banners'></home-swiper>
-        <recommend-view :recommends='recommends'></recommend-view>
-        <feature-view></feature-view>
-        <tab-control :titles="['流行','新款','精选']" class="tab-control" @tabClick='tabClick'></tab-control>
-        <goods-list :goods="goodsItem"></goods-list>
-        <ul>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-
-        </ul>
+        <scroll class="content" ref='homeScroll'>
+            <home-swiper :banners='banners'></home-swiper>
+            <recommend-view :recommends='recommends'></recommend-view>
+            <feature-view></feature-view>
+            <tab-control :titles="['流行','新款','精选']" class="tab-control" @tabClick='tabClick'></tab-control>
+            <goods-list :goods="goodsItem"></goods-list>
+        </scroll>
+        <!-- 监听组件的原生事件时,必需加上.native修饰符 -->
+        <back-top @click.native='backClick'></back-top>
     </div>
 </template>
 
@@ -49,9 +20,11 @@
     //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
     //例如：import 《组件名称》 from '《组件路径》';
     //common
+    import Scroll from '@/components/common/scroll/Scroll'
     import NavBar from '@/components/common/navbar/NavBar'
     import TabControl from '@/components/content/tabControl/TabControl'
     import GoodsList from '@/components/content/goods/GoodsList'
+    import BackTop from '@/components/content/backTop/BackTop'
     //child
     import HomeSwiper from '@/views/home/childComponents/HomeSwiper'
     import RecommendView from '@/views/home/childComponents/RecommendView'
@@ -71,9 +44,11 @@
     export default {
         //import引入的组件需要注入到对象中才能使用
         components: {
+            Scroll,
             NavBar,
             TabControl,
             GoodsList,
+            BackTop,
             HomeSwiper,
             RecommendView,
             FeatureView
@@ -111,6 +86,10 @@
              */
             tabClick(index) {
                 this.currentType = map.get(index)
+            },
+            backClick() {
+                // console.log('backClick')
+                this.$refs.homeScroll.scroll
             },
             /**
              * 接口方法
@@ -159,8 +138,9 @@
 </script>
 <style scoped>
     #home {
-        padding-top: 44px;
-
+        /* padding-top: 44px; */
+        /* 100 viewport height */
+        height: 100vh;
     }
 
     .home-nav {
@@ -176,9 +156,15 @@
     }
 
     .tab-control {
-        position: sticky;
+        /* position: sticky; */
         top: 44px;
 
         z-index: 9;
+    }
+
+    .content {
+        height: calc(100% - 93px);
+        margin-top: 44px;
+        overflow: hidden;
     }
 </style>
